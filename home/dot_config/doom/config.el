88f9e5a4b5c19366a3a56779d29de5ad80dc9bf9
lixp <golixp@outlook.com>
2026-01-31 21:53:07 +0800

    配置hugo导出

--- home/dot_config/doom/config.el
+++ home/dot_config/doom/config.el
@@ -152,3 +152,34 @@
             "* %U %?\n%a\n%U" :heading "Notes" :prepend t)
            ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file
             "* %U %?\n%a" :heading "Changelog" :prepend t))))
+
+;; hugo 导出配置
+(after! ox-hugo
+  ;; hugo 根目录
+  (setq! org-hugo-base-dir "~/project/blog")
+  ;; 自动写入最后修改时间
+  (setq! org-hugo-auto-set-lastmod t)
+  )
+
+
+;; 配置 org-roam 使用和 logseq 相同的路径和配置
+(after! org-roam
+  ;; 核心路径设置
+  (setq! org-roam-directory (file-truename "~/project/notes/")
+         org-roam-dailies-directory "journals/")
+
+  ;; 排除 Logseq 内部目录, 防止索引垃圾文件
+  (setq! org-roam-file-exclude-regexp (rx (or ".git/" "logseq/" "org/")))
+
+  ;; 配置普通文件模板
+  (setq! org-roam-capture-templates
+         '(("d" "default" plain "%?"
+            :target (file+head "pages/${slug}.org" "#+title: ${title}\n")
+            :unnarrowed t)))
+
+  ;; 配置日记文件模板
+  (setq! org-roam-dailies-capture-templates
+         '(("d" "default" entry "* %?"
+            :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  )
+
