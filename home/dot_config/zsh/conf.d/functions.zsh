8ac6df1f76e21cb451b414e17d791276983b49fc
lixp <golixp@outlook.com>
2026-06-07 20:35:34 +0800

    Update zsh proxy helpers

--- home/dot_config/zsh/conf.d/functions.zsh
+++ home/dot_config/zsh/conf.d/functions.zsh
@@ -11,12 +11,33 @@ proxy_on() {
 
     export http_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
     export https_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
-    export all_proxy="socks5://${PROXY_HOST}:${PROXY_PORT}"
+    export all_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
     export no_proxy="localhost,127.0.0.1,::1,10.0.0.0/8,192.168.0.0/16"
+    export HTTP_PROXY="$http_proxy"
+    export HTTPS_PROXY="$https_proxy"
+    export ALL_PROXY="$all_proxy"
+    export NO_PROXY="$no_proxy"
+
+    local -a node_options
+    node_options=(${=NODE_OPTIONS:-})
+    if (( ${node_options[(Ie)--use-env-proxy]} == 0 )); then
+        node_options+=(--use-env-proxy)
+    fi
+    export NODE_OPTIONS="${(j: :)node_options}"
 
     echo "Proxy ON → ${PROXY_HOST}:${PROXY_PORT}"
 }
 # 代理关闭
 proxy_off() {
     unset http_proxy https_proxy all_proxy no_proxy
+    unset HTTP_PROXY HTTPS_PROXY ALL_PROXY NO_PROXY
+
+    local -a node_options
+    node_options=(${=NODE_OPTIONS:-})
+    node_options=(${node_options:#--use-env-proxy})
+    if (( ${#node_options} > 0 )); then
+        export NODE_OPTIONS="${(j: :)node_options}"
+    else
+        unset NODE_OPTIONS
+    fi
 }
